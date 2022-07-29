83f66acc5cd163321460f08216179ed9901a62ab
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-07-29 11:36:49 -0300

    login ok

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -57,13 +57,18 @@ export class AuthService {
     console.log(payload.email)
     console.log(payload.password)
 
-    const headers = { 'Authorization': "Basic QVBJX05BTUVfQUNDRVNTOkFQSV9TRUNSRVRfQUNDRVNT", "Content-Type": "application/x-www-form-urlencoded" };
+    const headers = { 
+      'Authorization': "Basic QVBJX05BTUVfQUNDRVNTOkFQSV9TRUNSRVRfQUNDRVNT", 
+      "Content-Type": "application/x-www-form-urlencoded",
+      'Access-Control-Allow-Origin': '*'
+    };
     const Params = new URLSearchParams();
     Params.set('grant_type', 'password');
     //Params.set('username', 'nina@gmail.com');
     //Params.set('password', '123456');
     Params.set('username', payload.email);
     Params.set('password', payload.password);
+    console.log("@@@@@@@@@@@__________________________");
 
     return this.http.post<{ access_token: string, expires_in: string, jti: string, token_type: string }>(`${this.urlBase}${this.authorization}`, Params.toString(), { headers } ).pipe(
       map((res) => {
